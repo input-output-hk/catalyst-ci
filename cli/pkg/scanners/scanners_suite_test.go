54ff4961981d36d565fda0b0268fc2d135ad7626
Joshua Gilman <joshuagilman@gmail.com>
2023-05-31 15:14:40 -0700

    feat: adds CLI tool

---
+++ cli/pkg/scanners/scanners_suite_test.go
@@ -0,0 +1,24 @@
+package scanners_test
+
+import (
+	"testing"
+
+	"github.com/input-output-hk/catalyst-ci/cli/pkg"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+type mockParser struct {
+	earthfile pkg.Earthfile
+	err       error
+}
+
+func (m *mockParser) Parse(path string) (pkg.Earthfile, error) {
+	m.earthfile.Path = path
+	return m.earthfile, m.err
+}
+
+func TestFileScanner(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "FileScanner Suite")
+}
