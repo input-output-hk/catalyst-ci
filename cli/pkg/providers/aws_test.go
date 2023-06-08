e5725b279e909820b0fa0021b5b49bd00ad39cdc
Joshua Gilman <joshuagilman@gmail.com>
2023-06-07 17:21:30 -0700

    feat: adds support for fetching remote state from CLI

---
+++ cli/pkg/providers/aws_test.go
@@ -0,0 +1,87 @@
+package providers_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/input-output-hk/catalyst-ci/cli/pkg/providers"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+type mockS3Client struct {
+	s3iface.S3API
+	Response []byte
+	Err      error
+}
+
+func (m *mockS3Client) GetObject(
+	_ *s3.GetObjectInput,
+) (*s3.GetObjectOutput, error) {
+	if m.Err != nil {
+		return nil, m.Err
+	}
+
+	return &s3.GetObjectOutput{
+		Body: io.NopCloser(bytes.NewReader(m.Response)),
+	}, nil
+}
+
+var _ = Describe("Aws", func() {
+	Describe("Fetch", func() {
+		When("A valid response is returned", func() {
+			It("Should return the state", func() {
+				fetcher := providers.S3RemoteStateProvider{
+					Session: session.Must(session.NewSession()),
+					S3: &mockS3Client{
+						Response: []byte(
+							`{"version":1,"terraform_version":"1.0.0"}`,
+						),
+						Err: nil,
+					},
+				}
+
+				state, err := fetcher.Fetch("test-bucket", "test-key")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(state.Version).To(Equal(uint8(1)))
+				Expect(state.TerraformVersion).To(Equal("1.0.0"))
+			})
+		})
+
+		When("An invalid JSON response is returned", func() {
+			It("Should return an error", func() {
+				fetcher := providers.S3RemoteStateProvider{
+					Session: session.Must(session.NewSession()),
+					S3: &mockS3Client{
+						Response: []byte(
+							`{"version":1,"terraform_version":"1.0.0"`,
+						),
+						Err: nil,
+					},
+				}
+
+				_, err := fetcher.Fetch("test-bucket", "test-key")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		When("An error response is returned", func() {
+			It("Should return an error", func() {
+				fetcher := providers.S3RemoteStateProvider{
+					Session: session.Must(session.NewSession()),
+					S3: &mockS3Client{
+						Response: []byte(""),
+						Err:      fmt.Errorf("error"),
+					},
+				}
+
+				_, err := fetcher.Fetch("test-bucket", "test-key")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+})
---
+++ cli/pkg/state.go
@@ -0,0 +1,48 @@
+package pkg
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+/*
+ * State is an opportunistic representation of the terraform remote state file.
+ * It is not guaranteed to be complete, but it is sufficient for the purpose of
+ * extracting the outputs from the state file.
+ */
+type State struct {
+	Version          uint8             `json:"version"`
+	TerraformVersion string            `json:"terraform_version"`
+	Serial           uint64            `json:"serial"`
+	Lineage          string            `json:"lineage"`
+	Outputs          map[string]Output `json:"outputs"`
+	Resources        []any             `json:"resources"`
+	CheckResults     []any             `json:"check_results"`
+}
+
+// ParseOutput parses the output with the given key into the given value.
+func (s State) ParseOutput(key string, v interface{}) error {
+	output, ok := s.Outputs[key]
+	if !ok {
+		return fmt.Errorf("output %s not found in state", key)
+	}
+
+	return json.Unmarshal(output.Value, v)
+}
+
+/*
+ * Output is a representation of an output from a terraform state file. The
+ * value is kept as a json.RawMessage so that it can be unmarshalled into the
+ * appropriate type.
+ */
+type Output struct {
+	Value     json.RawMessage `json:"value"`
+	Type      string          `json:"type"`
+	Sensitive bool            `json:"sensitive,omitempty"`
+}
+
+// StateProvider is an interface for managing remote Terraform state files.
+type StateProvider interface {
+	// FetchState fetches the state file from the remote location.
+	Fetch(bucket string, key string) (State, error)
+}
