4c087fc482b9e818a97d99fba5679696cff674a6
Joshua Gilman <joshuagilman@gmail.com>
2023-10-09 10:23:28 -0700

    chore: fixes formatting

--- actions/discover/src/discover.ts
+++ actions/discover/src/discover.ts
@@ -14,9 +14,7 @@ export async function run(): Promise<void> {
     if (targets.trim() !== '') {
       flags.push(...targets.split(' ').map(t => `-t ${t}`))
     }
-    const command = ['ci', 'scan', ...flags, paths]
-      .filter(Boolean)
-      .join(' ')
+    const command = ['ci', 'scan', ...flags, paths].filter(Boolean).join(' ')
 
     core.info(`Running command: ${command}`)
     core.setOutput('json', await execCommand(command))
