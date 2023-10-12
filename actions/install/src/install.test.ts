ec9a284a639722d0f3ca694f2ae280a5a76dac18
Joshua Gilman <joshuagilman@gmail.com>
2023-10-12 14:52:54 -0700

    refactor: improves setup action (#36)

---
+++ actions/install/src/install.test.ts
@@ -0,0 +1,222 @@
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as github from '@actions/github'
+import { run } from './install'
+
+jest.mock('@actions/core', () => {
+  return {
+    getInput: jest.fn(),
+    info: jest.fn(),
+    setFailed: jest.fn()
+  }
+})
+jest.mock('@actions/tool-cache', () => ({
+  downloadTool: jest.fn(),
+  extractTar: jest.fn()
+}))
+jest.mock('@actions/github', () => ({
+  getOctokit: jest.fn()
+}))
+
+describe('Setup Action', () => {
+  const token = 'token'
+  const version = '1.0.0'
+
+  // actions core mocks
+  const getInputMock = core.getInput as jest.Mock
+  const setFailedMock = core.setFailed as jest.Mock
+
+  // actions tool-cache mocks
+  const downloadToolMock = tc.downloadTool as jest.Mock
+  const extractTarMock = tc.extractTar as jest.Mock
+
+  // actions github mocks
+  const getOctokitMock = github.getOctokit as jest.Mock
+
+  describe('when installing the CLI binary', () => {
+    describe('when the platform is not linux', () => {
+      it('should fail', async () => {
+        await run('darwin')
+
+        expect(setFailedMock).toHaveBeenCalledWith(
+          'This action only supports Linux runners'
+        )
+      })
+    })
+
+    describe('when the platform is linux', () => {
+      describe('when the version is invalid', () => {
+        beforeAll(() => {
+          getInputMock.mockImplementation((name: string) => {
+            switch (name) {
+              case 'token':
+                return token
+              case 'version':
+                return 'invalid'
+              default:
+                throw new Error(`Unknown input ${name}`)
+            }
+          })
+        })
+
+        it('should fail', async () => {
+          await run()
+          expect(setFailedMock).toHaveBeenCalledWith('Invalid version')
+        })
+      })
+
+      describe('when the version is valid', () => {
+        beforeAll(() => {
+          getInputMock.mockImplementation((name: string) => {
+            switch (name) {
+              case 'token':
+                return token
+              case 'version':
+                return version
+              default:
+                throw new Error(`Unknown input ${name}`)
+            }
+          })
+        })
+
+        describe('when the version does not exist', () => {
+          beforeAll(() => {
+            getOctokitMock.mockReturnValue({
+              rest: {
+                repos: {
+                  listReleases: jest.fn().mockResolvedValue({
+                    data: []
+                  })
+                }
+              }
+            })
+          })
+
+          it('should fail', async () => {
+            await run()
+            expect(setFailedMock).toHaveBeenCalledWith(
+              `Version ${version} not found`
+            )
+          })
+        })
+
+        describe('when the version exists', () => {
+          describe('when the assets is not found', () => {
+            beforeAll(() => {
+              getOctokitMock.mockReturnValue({
+                rest: {
+                  repos: {
+                    listReleases: jest.fn().mockResolvedValue({
+                      data: [
+                        {
+                          // eslint-disable-next-line camelcase
+                          tag_name: `v${version}`,
+                          assets: []
+                        }
+                      ]
+                    })
+                  }
+                }
+              })
+            })
+
+            it('should fail', async () => {
+              await run()
+              expect(setFailedMock).toHaveBeenCalledWith(
+                `Asset for version v${version} not found`
+              )
+            })
+          })
+
+          describe('when the assets is found', () => {
+            beforeAll(() => {
+              getOctokitMock.mockReturnValue({
+                rest: {
+                  repos: {
+                    listReleases: jest.fn().mockResolvedValue({
+                      data: [
+                        {
+                          // eslint-disable-next-line camelcase
+                          tag_name: `v2.0.0`,
+                          assets: [
+                            {
+                              name: 'cli-linux-amd64.tar.gz',
+                              // eslint-disable-next-line camelcase
+                              browser_download_url: 'https://example2.com'
+                            }
+                          ]
+                        },
+                        {
+                          // eslint-disable-next-line camelcase
+                          tag_name: `v${version}`,
+                          assets: [
+                            {
+                              name: 'cli-linux-amd64.tar.gz',
+                              // eslint-disable-next-line camelcase
+                              browser_download_url: 'https://example.com'
+                            }
+                          ]
+                        }
+                      ]
+                    })
+                  }
+                }
+              })
+            })
+
+            it('should download the asset', async () => {
+              await run()
+
+              expect(downloadToolMock).toHaveBeenCalledWith(
+                'https://example.com'
+              )
+            })
+
+            it('should extract the asset', async () => {
+              downloadToolMock.mockResolvedValue('/tmp/file.tar.gz')
+              await run()
+
+              expect(extractTarMock).toHaveBeenCalledWith(
+                '/tmp/file.tar.gz',
+                '/usr/local/bin'
+              )
+            })
+
+            describe('when the download fails', () => {
+              beforeAll(() => {
+                downloadToolMock.mockRejectedValue(new Error('Download error'))
+              })
+
+              it('should fail', async () => {
+                await run()
+                expect(setFailedMock).toHaveBeenCalledWith('Download error')
+              })
+            })
+
+            describe('when the version is latest', () => {
+              beforeAll(() => {
+                getInputMock.mockImplementation((name: string) => {
+                  switch (name) {
+                    case 'token':
+                      return token
+                    case 'version':
+                      return 'latest'
+                    default:
+                      throw new Error(`Unknown input ${name}`)
+                  }
+                })
+              })
+
+              it('should download the latest version', async () => {
+                await run()
+                expect(downloadToolMock).toHaveBeenCalledWith(
+                  'https://example2.com'
+                )
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+})
---
+++ actions/install/src/install.ts
@@ -0,0 +1,66 @@
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as github from '@actions/github'
+
+const assetName = 'cli-linux-amd64.tar.gz'
+const repoOwner = 'input-output-hk'
+const repoName = 'catalyst-ci'
+
+export async function run(
+  platform: NodeJS.Process['platform'] = process.platform
+): Promise<void> {
+  if (platform !== 'linux') {
+    core.setFailed('This action only supports Linux runners')
+    return
+  }
+
+  try {
+    const token = core.getInput('token')
+    const version = core.getInput('version')
+
+    if (version !== 'latest' && !isSemVer(version)) {
+      core.setFailed('Invalid version')
+      return
+    }
+
+    const octokit = github.getOctokit(token)
+    const { data: releases } = await octokit.rest.repos.listReleases({
+      owner: repoOwner,
+      repo: repoName
+    })
+
+    let targetRelease
+    if (version === 'latest') {
+      targetRelease = releases[0]
+    } else {
+      targetRelease = releases.find(r => r.tag_name === `v${version}`)
+    }
+
+    if (!targetRelease) {
+      core.setFailed(`Version ${version} not found`)
+      return
+    }
+
+    const asset = targetRelease.assets.find(a => a.name === assetName)
+    if (!asset) {
+      core.setFailed(`Asset for version v${version} not found`)
+      return
+    }
+
+    const finalURL = asset.browser_download_url
+    core.info(`Downloading version ${version} from ${finalURL}`)
+    const downloadPath = await tc.downloadTool(finalURL)
+    const extractPath = await tc.extractTar(downloadPath, '/usr/local/bin')
+    core.info(`Installed cli to ${extractPath}`)
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed('Unknown error')
+    }
+  }
+}
+
+function isSemVer(version: string): boolean {
+  return /^\d+\.\d+\.\d+$/.test(version)
+}
