3b56337e575d381cf2ccabc65f016aa83829274b
Joshua Gilman <joshuagilman@gmail.com>
2023-10-13 11:47:44 -0700

    feat: adds push action

---
+++ actions/push/src/push.ts
@@ -0,0 +1,28 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+export async function run(): Promise<void> {
+  try {
+    const image = core.getInput('image')
+    const registries = core.getInput('registries').split('\n')
+    const tags = core.getInput('tags').split('\n')
+
+    for (const registry of registries) {
+      for (const tag of tags) {
+        const fullImage = `${registry}/${image}:${tag}`
+
+        core.info(`Tagging ${image} as ${fullImage}`)
+        await exec.exec('docker', ['tag', image, fullImage])
+
+        core.info(`Pushing ${fullImage}`)
+        await exec.exec('docker', ['push', fullImage])
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed('Unknown error')
+    }
+  }
+}
