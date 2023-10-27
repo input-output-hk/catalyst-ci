0eb3ab0142dfa6aa9dc65755b636c2673bc38c57
Steven Johnson <sakurainds@gmail.com>
2023-10-27 16:08:29 +0700

    feat(rust): Rework rust builders to handle hosts with differeing cpu architectures.

---
+++ earthly/rust/example/README.md
@@ -0,0 +1,7 @@
+# Example Rust Project
+
+This is an simple but hopefully full featured example rust project.
+It is used to test both our Catalyst-CI Rust building infrastructure and serve as a template for its use.
+
+This code was originally sourced from: ["rust-starter](https://rust-starter.github.io/).
+The code in this repo has been adapted for our needs.
---
+++ earthly/rust/scripts/std_checks.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# This script is run inside the `check` stage for rust projects to perfom all 
+# high level non-compilation checks.
+# These are the Standard checks which ALL rust targets must pass before they
+# will be scheduled to be `buuild`. 
+# Individual targets can add extra `check` steps, but these checks must always
+# pass. 
+
+source "$(dirname "$0")/colors.sh"
+
+status() {
+    local rc="$1"
+    local message="$2"
+    shift 2
+
+    # Check if the command returned a bad status
+    if "$@"; then
+        # Append green OK to the message
+        echo -e "${CYAN}${message} : ${GREEN}[OK]${NC}"
+    else
+        # Append red ERROR to the message
+        echo -e "${CYAN}${message} : ${RED}[ERROR]${NC}"
+        rc=1
+    fi
+
+    # Return the current status
+    return "$rc"
+}
+
+# Checks if two files that should exist DO, and are equal.
+# used to enforce consistency between local config files and the expected config locked in CI.
+check_vendored_files() {
+    local rc=$1
+    local localfile=$2
+    local vendorfile=$3
+
+    status "$rc" "Checking if Local File '$localfile' == Vendored File '$vendorfile'" \
+        colordiff -Naur "$localfile" "$vendorfile"
+    return $?
+}
+
+# This is set up so that ALL checks are run and it will fail if any fail.
+# This imporvies visibility into all issues that need to be corrected for `check`
+# to pass without needing to iterate excessively.
+
+rc=0
+
+## Check if .cargo.config.toml has been modified.
+check_vendored_files $rc .cargo/config.toml "$CARGO_HOME"/config.toml; rc=$?
+check_vendored_files $rc rustfmt.toml /stdcfgs/rustfmt.toml; rc=$?
+
+# Check if the rust src is properly formatted.
+status $rc "Checking Rust Code Format" cargo +nightly fmtchk; rc=$?
+if [ $rc -ne 0 ]; then
+    echo -e "    ${YELLOW}You can locally fix format errors by running: \`cargo +nightly fmtfix\`${NC}"
+fi
+
+# Check if we have unused dependencies declared in our Cargo.toml files.
+status $rc "Checking for Unused Dependencies" cargo machete; rc=$?
+
+# Return an error if any of this fails.
+exit $rc
