5d08f2c77c092b310bb65e69922e4fb1bfc8aa84
Mr-Leshiy <leshiy12345678@gmail.com>
2023-11-02 17:16:33 +0200

    add `privilaged` flag for actions/run

--- actions/run/src/run.ts
+++ actions/run/src/run.ts
@@ -8,6 +8,7 @@ export async function run(): Promise<void> {
   const earthfile = core.getInput('earthfile')
   const flags = core.getInput('flags')
   const platform = core.getInput('platform')
+  const privileged = core.getBooleanInput('privileged')
   const runnerAddress = core.getInput('runner_address')
   const runnerPort = core.getInput('runner_port')
   const target = core.getInput('target')
@@ -16,7 +17,9 @@ export async function run(): Promise<void> {
   const command = 'earthly'
   const args: string[] = []
 
-  args.push('-P')
+  if (privileged) {
+    args.push('-P')
+  }
 
   if (runnerAddress) {
     args.push('--buildkit-host', `tcp://${runnerAddress}:${runnerPort}`)
