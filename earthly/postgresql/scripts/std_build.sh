4ff45db7c185e4b35151912208ea8e56b9fd1bf5
Steven Johnson <sakurainds@gmail.com>
2023-11-28 17:19:45 +0700

    feat(postgresql): Build now ensures the schema can apply cleanly, and that all seed data is valid,

--- earthly/postgresql/scripts/std_build.sh
+++ earthly/postgresql/scripts/std_build.sh
@@ -8,22 +8,45 @@ basedir=$(dirname "$0")
 
 source "${basedir}/include/colors.sh"
 source "${basedir}/db_ops.sh"
+ 
+setup_and_migrate() {
+    local reason="$1"
+    shift 1
+
+    # Setup the base db namespace
+    status_and_exit "${reason}: DB Setup" \
+        setup_db ./setup-db.sql "$@" --dbname=test --dbdesc="Test DB"
+
+    # Run all migrations
+    status_and_exit "${reason}: DB Migrate" \
+        migrate_schema "$@" --dbname=test
+}
 
 # Init the db data in a tmp place
 status_and_exit "DB Initial Setup" \
-    init_db /tmp/data
+    init_db "$@" --dbpath="/tmp/data"
 
 # Start the db server
 status_and_exit "DB Start" \
-    run_pgsql /tmp/data 10
+    run_pgsql "$@" --dbpath="/tmp/data"
 
-# Setup the base db namespace
-status_and_exit "DB Setup" \
-    setup_db ./setup-db.sql test "Test DB" test test
+# Start the db server
+status_and_exit "DB Ready" \
+    wait_ready_pgsql "$@" --dbreadytimeout="10" 
 
-# Run all migrations
-status_and_exit "DB Migrate" \
-    migrate_schema test localhost 5432 test test
+# Setup Schema and run migrations.
+setup_and_migrate "Initilization" "$@"
+
+# Test each seed data set can apply cleanly
+rc=0
+while IFS= read -r -d '' file
+do
+    status "${rc}" "Applying seed data from ${file}" \
+        apply_seed_data "${file}" --dbname=test; rc=$?
+
+    # Reset schema so all seed data get applied to a clean database.
+    setup_and_migrate "Reset" "$@"
+done < <(find ./seed/* -maxdepth 1 -type d -print0 | sort -z)
 
 # Stop the database
 status_and_exit "DB Stop" \
@@ -33,4 +56,4 @@ status_and_exit "DB Stop" \
 rm -rf /tmp/data
 
 # These tests will immeditaley fail if the DB is not setup properly.
-exit 0
+exit "${rc}"
