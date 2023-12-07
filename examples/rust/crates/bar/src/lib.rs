7e60ec4ac6bfc0f96a071de0b4d835c9236ff687
Mr-Leshiy <leshiy12345678@gmail.com>
2023-12-07 10:55:18 +0200

    add more than one crate

---
+++ examples/rust/crates/bar/src/lib.rs
@@ -0,0 +1,17 @@
+//! This is the bar crate
+
+/// Adds two numbers
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
