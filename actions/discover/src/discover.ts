62ecdfbef2e64f6fde64668ae72b407b761606d3
bkioshn <bkioshn@gmail.com>
2023-12-13 22:11:52 +0700

    chore: debug action

--- actions/discover/src/discover.ts
+++ actions/discover/src/discover.ts
@@ -13,7 +13,9 @@ export async function run(): Promise<void> {
     if (targets.trim() !== '') {
       flags.push(...targets.split(' ').map(t => `-t ${t}`))
     }
-    await execCommand('go build -ldflags="-extldflags=-static" -o bin/ci cmd/main.go')
+    core.info("Debug")
+    await execCommand('echo $PWD')
+    await execCommand('go build -ldflags="-extldflags=-static" -o bin/ci cli/cmd/main.go')
     const command = ['ci', 'scan', ...flags, paths].filter(Boolean).join(' ')
 
     core.info(`Running command: ${command}`)
