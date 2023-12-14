57bda0f0f4abe7f93e453ac174420add169316b8
bkioshn <bkioshn@gmail.com>
2023-12-14 15:38:23 +0700

    chore: debug install

--- actions/install/src/install.ts
+++ actions/install/src/install.ts
@@ -15,62 +15,62 @@ export async function run(
     return
   }
 
-  core.info('Debug')
-  await exec('ls', (err, stdout, stderr) => {
-    if (err || stderr) {
-      console.error(`exec error: ${err}`)
+  // core.info('Debug')
+  // await exec('ls', (err, stdout, stderr) => {
+  //   if (err || stderr) {
+  //     console.error(`exec error: ${err}`)
+  //     return
+  //   }
+
+  //   console.log(`Show ${stdout}`)
+  // })
+  // return
+
+  try {
+    const token = core.getInput('token')
+    const version = core.getInput('version')
+
+    if (version !== 'latest' && !isSemVer(version)) {
+      core.setFailed('Invalid version')
       return
     }
 
-    console.log(`Show ${stdout}`)
-  })
-  return
+    const octokit = github.getOctokit(token)
+    const { data: releases } = await octokit.rest.repos.listReleases({
+      owner: repoOwner,
+      repo: repoName
+    })
 
-  // try {
-  //   const token = core.getInput('token')
-  //   const version = core.getInput('version')
+    let targetRelease
+    if (version === 'latest') {
+      targetRelease = releases[0]
+    } else {
+      targetRelease = releases.find(r => r.tag_name === `v${version}`)
+    }
 
-  //   if (version !== 'latest' && !isSemVer(version)) {
-  //     core.setFailed('Invalid version')
-  //     return
-  //   }
+    if (!targetRelease) {
+      core.setFailed(`Version ${version} not found`)
+      return
+    }
 
-  //   const octokit = github.getOctokit(token)
-  //   const { data: releases } = await octokit.rest.repos.listReleases({
-  //     owner: repoOwner,
-  //     repo: repoName
-  //   })
+    const asset = targetRelease.assets.find(a => a.name === assetName)
+    if (!asset) {
+      core.setFailed(`Asset for version v${version} not found`)
+      return
+    }
 
-  //   let targetRelease
-  //   if (version === 'latest') {
-  //     targetRelease = releases[0]
-  //   } else {
-  //     targetRelease = releases.find(r => r.tag_name === `v${version}`)
-  //   }
-
-  //   if (!targetRelease) {
-  //     core.setFailed(`Version ${version} not found`)
-  //     return
-  //   }
-
-  //   const asset = targetRelease.assets.find(a => a.name === assetName)
-  //   if (!asset) {
-  //     core.setFailed(`Asset for version v${version} not found`)
-  //     return
-  //   }
-
-  //   const finalURL = asset.browser_download_url
-  //   core.info(`Downloading version ${version} from ${finalURL}`)
-  //   const downloadPath = await tc.downloadTool(finalURL)
-  //   const extractPath = await tc.extractTar(downloadPath, '/usr/local/bin')
-  //   core.info(`Installed cli to ${extractPath}`)
-  // } catch (error) {
-  //   if (error instanceof Error) {
-  //     core.setFailed(error.message)
-  //   } else {
-  //     core.setFailed('Unknown error')
-  //   }
-  // }
+    const finalURL = asset.browser_download_url
+    core.info(`Downloading version ${version} from ${finalURL}`)
+    const downloadPath = await tc.downloadTool(finalURL)
+    const extractPath = await tc.extractTar(downloadPath, '/usr/local/bin')
+    core.info(`Installed cli to ${extractPath}`)
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed('Unknown error')
+    }
+  }
 }
 
 function isSemVer(version: string): boolean {
