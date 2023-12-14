c07786240381bdd7159150f3213c234618ee3fb4
bkioshn <bkioshn@gmail.com>
2023-12-14 14:49:48 +0700

    chore: hotfix show ls

--- actions/discover/src/discover.ts
+++ actions/discover/src/discover.ts
@@ -12,6 +12,8 @@ export async function run(): Promise<void> {
     if (targets.trim() !== '') {
       flags.push(...targets.split(' ').map(t => `-t ${t}`))
     }
+    core.info("see ls")
+    execCommand("ls")
     const command = ['ci', 'scan', ...flags, paths].filter(Boolean).join(' ')
 
     core.info(`Running command: ${command}`)
