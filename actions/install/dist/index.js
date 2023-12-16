cab6ec2c8dc1f19c3bda9405a1273f5b5024a8eb
bkioshn <bkioshn@gmail.com>
2023-12-16 12:21:16 +0700

    test: try remove cd

--- actions/install/dist/index.js
+++ actions/install/dist/index.js
@@ -13737,7 +13737,7 @@ async function run(platform = process.platform) {
         core.info(`> local ${local}`);
         if (local === 'true') {
             core.info('Local flag is used');
-            (0,external_child_process_.exec)('cd cli && go build -ldflags="-extldflags=-static" -o bin/ci  cmd/main.go', (error, stdout, stderr) => {
+            (0,external_child_process_.exec)('go build -ldflags="-extldflags=-static" -o bin/ci  cmd/main.go', (error, stdout, stderr) => {
                 if (error || stderr) {
                     console.log(new Error(error ? error.message : stderr));
                 }
--- actions/install/src/install.ts
+++ actions/install/src/install.ts
@@ -23,7 +23,7 @@ export async function run(
     core.info(`> local ${local}`)
     if (local === 'true') {
       core.info('Local flag is used')
-      exec('cd cli && go build -ldflags="-extldflags=-static" -o bin/ci  cmd/main.go', (error, stdout, stderr) => {
+      exec('go build -ldflags="-extldflags=-static" -o bin/ci  cmd/main.go', (error, stdout, stderr) => {
         if (error || stderr) {
           console.log(new Error(error ? error.message : stderr))
         } else {
