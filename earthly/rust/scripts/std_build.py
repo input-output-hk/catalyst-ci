6422e51bc5d823b497d0e7ecfe1c7d19362ce3ec
Mr-Leshiy <leshiy12345678@gmail.com>
2024-01-04 14:42:04 +0200

    add target flag to std_build.py

--- earthly/rust/scripts/std_build.py
+++ earthly/rust/scripts/std_build.py
@@ -18,39 +18,41 @@ def main():
     parser = argparse.ArgumentParser(
         description="Rust build processing."
     )
-    parser.add_argument("--cov_report", help="The output coverage report file path.")
-    parser.add_argument("--libs", help="The list of lib crates `cargo-modules` docs to build separated by comma.")
-    parser.add_argument("--bins", help="The list of binaries `cargo-modules` docs to build.")
+    parser.add_argument("--target", default="", help="Rust target value.")
+    parser.add_argument("--cov_report", default="", help="The output coverage report file path.")
+    parser.add_argument("--libs", default="", help="The list of lib crates `cargo-modules` docs to build separated by comma.")
+    parser.add_argument("--bins", default="", help="The list of binaries `cargo-modules` docs to build.")
     args = parser.parse_args()
 
     results = cli.Results("Rust builds")
 
-    # Build the code.
-    results.add(cli.run("cargo build --release --workspace --locked", name="Build all code in the workspace"))
-    # # Check the code passes all clippy lint checks.
-    # results.add(cli.run("cargo lint", name="Clippy Lints in the workspace check"))
-    # Check we can generate all the documentation.
-    results.add(cli.run("cargo docs", name="Documentation can be generated OK check"))
-    # Check if all documentation tests pass.
-    results.add(cli.run("cargo testdocs", name="Documentation tests all pass check"))
-    # Check if any benchmarks defined run (We don;t validate the results.)
-    results.add(cli.run("cargo bench --all-targets", name="Benchmarks all run to completion check"))
+    target_flag = ""
+    if args.target != "":
+        target_flag = f"--target={args.target}"
 
-    # Run unit tests and generates test and coverage report artifacts
-    res = cli.run("cargo llvm-cov clean --workspace", name="Remove artifacts that may affect the coverage results")
-    results.add(res)
-    if res.ok():
-        res = cli.run("cargo llvm-cov nextest --release --bins --lib --workspace --locked -P ci",
-            name="Run unit tests and display test result and test coverage")
-        if not res.ok():
-            print("[yellow]You can locally run tests by running: [/yellow] \n [red bold]cargo testunit[/red bold]")
-        results.add(res)
+    # Build the code.
+    results.add(cli.run(f"cargo build {target_flag} --release --workspace --locked", name="Build all code in the workspace"))
+    # Check the code passes all clippy lint checks.
+    results.add(cli.run(f"cargo lint {target_flag}", name="Clippy Lints in the workspace check"))
+    # Check if all Self contained tests pass (Test that need no external resources).
+    results.add(cli.run(f"cargo testunit {target_flag}", name="Self contained Unit tests all pass check"))
+    # Check we can generate all the documentation.
+    results.add(cli.run(f"cargo docs {target_flag}", name="Documentation can be generated OK check"))
+    # Check if all documentation tests pass.
+    results.add(cli.run(f"cargo testdocs {target_flag}", name="Documentation tests all pass check"))
+    # Check if any benchmarks defined run (We don;t validate the results.)
+    results.add(cli.run(f"cargo bench --all-targets {target_flag}", name="Benchmarks all run to completion check"))
 
     # Save coverage report to file if it is provided
-    if res.ok() and args.cov_report != "":
-        res = cli.run(f"cargo llvm-cov report --release --output-path {args.cov_report}",
-            name=f"Generate lcov report to {args.cov_report}")
+    if args.cov_report != "":
+        # Remove artifacts that may affect the coverage results
+        res = cli.run("cargo llvm-cov clean --workspace", name="Remove artifacts that may affect the coverage results")
         results.add(res)
+        # Save coverage report to file if it is provided
+        if res.ok():
+            res = cli.run(f"cargo llvm-cov report --release {target_flag} --output-path {args.cov_report}",
+                name=f"Generate lcov report to {args.cov_report}")
+            results.add(res)
 
     # Generate dependency graphs
     results.add(cli.run("cargo depgraph --workspace-only --dedup-transitive-deps > target/doc/workspace.dot",
