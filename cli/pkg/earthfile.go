9b6eec9938ef4a738ad058b08a45ff8e9e90c3ed
vasapornchaiyakul <134899205+vasapornchaiyakul@users.noreply.github.com>
2024-01-10 11:41:47 +0700

    Update earthfile.go

--- cli/pkg/earthfile.go
+++ cli/pkg/earthfile.go
@@ -1,121 +1,96 @@
-package spec
+package pkg
 
-// Earthfile is the AST representation of an Earthfile.
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/earthly/earthly/ast/spec"
+)
+
+// Earthfile represents a parsed Earthfile.
 type Earthfile struct {
-	Version        *Version        `json:"version,omitempty"`
-	BaseRecipe     Block           `json:"baseRecipe"`
-	Targets        []Target        `json:"targets,omitempty"`
-	Functions      []Function      `json:"functions,omitempty"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
+	BaseRecipe     spec.Block           `json:"baseRecipe"`
+	Path           string               `json:"path,omitempty"`
+	SourceLocation *spec.SourceLocation `json:"sourceLocation,omitempty"`
+	Targets        []spec.Target        `json:"targets,omitempty"`
+	UserCommands   []spec.UserCommand   `json:"userCommands,omitempty"`
+	Version        *spec.Version        `json:"version,omitempty"`
 }
 
-// Target is the AST representation of an Earthfile target.
-type Target struct {
-	Name           string          `json:"name"`
-	Docs           string          `json:"docs,omitempty"`
-	Recipe         Block           `json:"recipe"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
+// Earthfile and its filtered targets.
+type EarthTargets struct {
+	Earthfile Earthfile
+	Targets   []string
 }
 
-// Function is the AST representation of an Earthfile function definition.
-type Function struct {
-	Name           string          `json:"name"`
-	Recipe         Block           `json:"recipe"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
+func (e Earthfile) GetImages(target string) ([]string, error) {
+	commands, err := e.GetCommands(target, "SAVE IMAGE")
+	if err != nil {
+		return nil, err
+	}
+
+	if len(commands) == 0 {
+		return nil, nil
+	}
+
+	var images []string
+	for _, command := range commands {
+		for _, arg := range command.Args {
+			if !strings.HasPrefix(arg, "--") {
+				// Remove any variables from the string
+				re := regexp.MustCompile(`\$\{[^}]+\}`)
+				processedStr := re.ReplaceAllString(arg, "")
+
+				// Remove any tags from the string
+				imageName := strings.Split(processedStr, ":")[0]
+
+				images = append(images, imageName)
+			}
+		}
+	}
+
+	return images, nil
 }
 
-// Version is the AST representation of an Earthfile version definition.
-type Version struct {
-	Args           []string        `json:"args"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
+func (e Earthfile) GetCommands(
+	target string,
+	command string,
+) ([]*spec.Command, error) {
+	var commands []*spec.Command
+	t, err := e.GetTarget(target)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, statement := range t.Recipe {
+		if statement.Command != nil && statement.Command.Name == command {
+			commands = append(commands, statement.Command)
+		}
+	}
+
+	return commands, nil
 }
 
-// Block is the AST representation of a block of statements.
-type Block []Statement
-
-// Statement is the AST representation of an Earthfile statement. Only one field may be
-// filled at one time.
-type Statement struct {
-	Command        *Command        `json:"command,omitempty"`
-	With           *WithStatement  `json:"with,omitempty"`
-	If             *IfStatement    `json:"if,omitempty"`
-	Try            *TryStatement   `json:"try,omitempty"`
-	For            *ForStatement   `json:"for,omitempty"`
-	Wait           *WaitStatement  `json:"wait,omitempty"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
+func (e Earthfile) GetTarget(target string) (*spec.Target, error) {
+	for _, t := range e.Targets {
+		if t.Name == target {
+			return &t, nil
+		}
+	}
+	return nil, fmt.Errorf("target %s not found in %s", target, e.Path)
 }
 
-// Command is the AST representation of an Earthfile command.
-type Command struct {
-	Name           string          `json:"name"`
-	Docs           string          `json:"docs,omitempty"`
-	Args           []string        `json:"args"`
-	ExecMode       bool            `json:"execMode,omitempty"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
+// EarthfileParser is an interface that can parse Earthfiles.
+type EarthfileParser interface {
+	Parse(path string) (Earthfile, error)
 }
 
-func (c Command) Clone() Command {
-	newCmd := c
-	args := make([]string, len(c.Args))
-	copy(args, c.Args)
-	newCmd.Args = args
-	srcLoc := *c.SourceLocation
-	newCmd.SourceLocation = &srcLoc
-	return newCmd
-}
+// EarthfileScanner is an interface that can scan for Earthfiles.
+type EarthfileScanner interface {
+	// Scan returns a list of Earthfiles.
+	Scan() ([]Earthfile, error)
 
-// WithStatement is the AST representation of a with statement.
-type WithStatement struct {
-	Command        Command         `json:"command"`
-	Body           Block           `json:"body"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
-}
-
-// IfStatement is the AST representation of an if statement.
-type IfStatement struct {
-	Expression     []string        `json:"expression"`
-	ExecMode       bool            `json:"execMode,omitempty"`
-	IfBody         Block           `json:"ifBody"`
-	ElseIf         []ElseIf        `json:"elseIf,omitempty"`
-	ElseBody       *Block          `json:"elseBody,omitempty"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
-}
-
-// TryStatement is the AST representation of a try statement.
-type TryStatement struct {
-	TryBody        Block           `json:"tryBody"`
-	CatchBody      *Block          `json:"catchBody,omitempty"`
-	FinallyBody    *Block          `json:"finallyBody,omitempty"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
-}
-
-// ElseIf is the AST representation of an else if clause.
-type ElseIf struct {
-	Expression     []string        `json:"expression"`
-	ExecMode       bool            `json:"execMode,omitempty"`
-	Body           Block           `json:"body"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
-}
-
-// ForStatement is the AST representation of a for statement.
-type ForStatement struct {
-	Args           []string        `json:"args"`
-	Body           Block           `json:"body"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
-}
-
-// WaitStatement is the AST representation of a for statement.
-type WaitStatement struct {
-	Args           []string        `json:"args"`
-	Body           Block           `json:"body"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
-}
-
-// SourceLocation is an optional reference to the original source code location.
-type SourceLocation struct {
-	File        string `json:"file,omitempty"`
-	StartLine   int    `json:"startLine"`
-	StartColumn int    `json:"startColumn"`
-	EndLine     int    `json:"endLine"`
-	EndColumn   int    `json:"endColumn"`
+	// ScanForTarget returns a list of Earthfiles that contain the given target.
+	ScanForTarget(target string) ([]Earthfile, error)
 }
