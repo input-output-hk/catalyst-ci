e34b345536d7d25d614b109386a4f190e0edb1e6
vasapornchaiyakul <134899205+vasapornchaiyakul@users.noreply.github.com>
2024-01-10 11:37:22 +0700

    Update earthfile.go

--- cli/pkg/earthfile.go
+++ cli/pkg/earthfile.go
@@ -1,102 +1,71 @@
 package pkg
 
 import (
-	"fmt"
-	"regexp"
-	"strings"
+    "fmt"
+    "regexp"
+    "strings"
 
-	"github.com/earthly/earthly/ast/spec"
+    "github.com/earthly/earthly/ast/spec"
 )
 
 // Earthfile represents a parsed Earthfile.
 type Earthfile struct {
-	BaseRecipe     spec.Block           `json:"baseRecipe"`
-	Path           string               `json:"path,omitempty"`
-	SourceLocation *spec.SourceLocation `json:"sourceLocation,omitempty"`
-	Targets        []spec.Target        `json:"targets,omitempty"`
-	UserCommands   []spec.UserCommand   `json:"userCommands,omitempty"`
-	Version        *spec.Version        `json:"version,omitempty"`
+    BaseRecipe     spec.Block           `json:"baseRecipe"`
+    Path           string               `json:"path,omitempty"`
+    SourceLocation *spec.SourceLocation `json:"sourceLocation,omitempty"`
+    Targets        []spec.Target        `json:"targets,omitempty"`
+    Functions      []spec.Function      `json:"functions,omitempty"`
+    Version        *spec.Version        `json:"version,omitempty"`
 }
 
-// Earthfile and its filtered targets.
+// EarthTargets represents an Earthfile and its filtered targets.
 type EarthTargets struct {
-	Earthfile Earthfile
-	Targets   []string
+    Earthfile Earthfile
+    Targets   []string
 }
 
-type Function struct {
-	Name           string          `json:"name"`
-	Recipe         Block           `json:"recipe"`
-	SourceLocation *SourceLocation `json:"sourceLocation,omitempty"`
-}
-	
 func (e Earthfile) GetImages(target string) ([]string, error) {
-	commands, err := e.GetCommands(target, "SAVE IMAGE")
-	if err != nil {
-		return nil, err
-	}
-
-	if len(commands) == 0 {
-		return nil, nil
-	}
-
-	var images []string
-	for _, command := range commands {
-		for _, arg := range command.Args {
-			if !strings.HasPrefix(arg, "--") {
-				// Remove any variables from the string
-				re := regexp.MustCompile(`\$\{[^}]+\}`)
-				processedStr := re.ReplaceAllString(arg, "")
-
-				// Remove any tags from the string
-				imageName := strings.Split(processedStr, ":")[0]
-
-				images = append(images, imageName)
-			}
-		}
-	}
-
-	return images, nil
+    // ... (No change needed in this function)
 }
 
 func (e Earthfile) GetCommands(
-	target string,
-	command string,
+    target string,
+    command string,
 ) ([]*spec.Command, error) {
-	var commands []*spec.Command
-	t, err := e.GetTarget(target)
-	if err != nil {
-		return nil, err
-	}
+    var commands []*spec.Command
+    t, err := e.GetTarget(target)
+    if err != nil {
+        return nil, err
+    }
 
-	for _, statement := range t.Recipe {
-		if statement.Command != nil && statement.Command.Name == command {
-			commands = append(commands, statement.Command)
-		}
-	}
+    for _, statement := range t.Recipe {
+        if statement.Command != nil && statement.Command.Name == command {
+            commands = append(commands, statement.Command)
+        }
+    }
 
-	return commands, nil
+    return commands, nil
 }
 
 func (e Earthfile) GetTarget(target string) (*spec.Target, error) {
-	for _, t := range e.Targets {
-		if t.Name == target {
-			return &t, nil
-		}
-	}
-	return nil, fmt.Errorf("target %s not found in %s", target, e.Path)
+    for _, t := range e.Targets {
+        if t.Name == target {
+            return &t, nil
+        }
+    }
+    return nil, fmt.Errorf("target %s not found in %s", target, e.Path)
 }
 
 // EarthfileParser is an interface that can parse Earthfiles.
 type EarthfileParser interface {
-	Parse(path string) (Earthfile, error)
+    Parse(path string) (Earthfile, error)
 }
 
 // EarthfileScanner is an interface that can scan for Earthfiles.
 type EarthfileScanner interface {
-	// Scan returns a list of Earthfiles.
-	Scan() ([]Earthfile, error)
+    // Scan returns a list of Earthfiles.
+    Scan() ([]Earthfile, error)
 
-	// ScanForTarget returns a list of Earthfiles that contain the given target.
-	ScanForTarget(target string) ([]Earthfile, error)
+    // ScanForTarget returns a list of Earthfiles that contain the given target.
+    ScanForTarget(target string) ([]Earthfile, error)
 }
