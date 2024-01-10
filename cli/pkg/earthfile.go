e813ae6bca9d1f4b0aef87f51377dd5fb4cd5fd8
vasapornchaiyakul <134899205+vasapornchaiyakul@users.noreply.github.com>
2024-01-10 12:25:05 +0700

    Update earthfile.go

--- cli/pkg/earthfile.go
+++ cli/pkg/earthfile.go
@@ -14,7 +14,7 @@ type Earthfile struct {
 	Path           string               `json:"path,omitempty"`
 	SourceLocation *spec.SourceLocation `json:"sourceLocation,omitempty"`
 	Targets        []spec.Target        `json:"targets,omitempty"`
-	Functions      []spec.Function      `json:"Functions,omitempty"`
+	UserCommands   []spec.UserCommand   `json:"userCommands,omitempty"`
 	Version        *spec.Version        `json:"version,omitempty"`
 }
 
