e6157c0f1482c1639b045ea03023f9353de7a954
bkioshn <bkioshn@gmail.com>
2024-01-12 08:30:03 +0700

    fix: var

--- earthly/spectral/minify-json.sh
+++ earthly/spectral/minify-json.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 # Minify all JSON in the directory if the file type is json
-if [[ "${FILE_TYPE}" == "json" ]]; then 
-    # Loop through each JSON file in the directory
-    for json_file in "${DIR}"/*.json; do 
-        # Check if the file is a regular file
-        if [[ -f "${json_file}" ]]; then 
-            # Minify the JSON file using jq and overwrite the original file
-            jq -c . "${json_file}" > "${json_file}.tmp" && mv "${json_file}.tmp" "${json_file}" 
-            echo "Compacted: ${json_file}" 
-        fi 
-    done 
+# Check if FILE_TYPE and DIR are not empty
+if [ -n "${file_type}" ] && [ -n "${dir}" ]; then
+    # Minify all JSON in the directory if the file type is json
+    if [ "${file_type}" == "json" ]; then 
+        # Loop through each JSON file in the directory
+        for json_file in "${dir}"/*.json; do 
+            # Check if the file is a regular file
+            if [ -f "${json_file}" ]; then 
+                # Minify the JSON file using jq and overwrite the original file
+                jq -c . "${json_file}" > "${json_file}.tmp" && mv "${json_file}.tmp" "${json_file}" 
+                echo "Compacted: ${json_file}" 
+            fi 
+        done 
+    else 
+        echo "File type is not JSON." 
+    fi 
 else 
-    echo "File type is not JSON." 
-fi 
+    echo "FILE_TYPE or DIR is empty."
+fi
