2da70d1f99fde3906875f5d90cdc504e77bffb86
bkioshn <bkioshn@gmail.com>
2024-01-12 07:47:21 +0700

    fix: bash format

--- earthly/spectral/minify-json.sh
+++ earthly/spectral/minify-json.sh
@@ -1,12 +1,12 @@
 # Minify all JSON in the directory if the file type is json
-if [ "$file_type" == "json" ]; then 
+if [[ "${file_type}" == "json" ]]; then 
     # Loop through each JSON file in the directory
-    for json_file in "$dir"/*.json; do 
+    for json_file in "${dir}"/*.json; do 
         # Check if the file is a regular file
-        if [ -f "$json_file" ]; then 
+        if [[ -f "${json_file}" ]]; then 
             # Minify the JSON file using jq and overwrite the original file
-            jq -c . "$json_file" > "$json_file.tmp" && mv "$json_file.tmp" "$json_file" 
-            echo "Compacted: $json_file" 
+            jq -c . "${json_file}" > "${json_file}.tmp" && mv "${json_file}.tmp" "${json_file}" 
+            echo "Compacted: ${json_file}" 
         fi 
     done 
 else 
