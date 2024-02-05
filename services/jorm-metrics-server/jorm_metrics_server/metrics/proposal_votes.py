a189b4e01018d492f15c208ab55b06d5338627d8
Joshua Gilman <joshuagilman@gmail.com>
2024-02-04 17:48:19 -0800

    chore: adds explanatory comments

--- services/jorm-metrics-server/jorm_metrics_server/metrics/proposal_votes.py
+++ services/jorm-metrics-server/jorm_metrics_server/metrics/proposal_votes.py
@@ -29,6 +29,7 @@ async def scrape(client: ApiClient):
     plans = await client.get_active_plans()
     voters = await client.get_voters()
 
+    # Index proposals by vote plan ID and proposal index
     proposals_by_index = {}
     for proposal in proposals.values():
         if proposal.chain_voteplan_id not in proposals_by_index:
@@ -38,6 +39,8 @@ async def scrape(client: ApiClient):
             proposal.chain_proposal_index
         ] = proposal
 
+    # Calculate the voting power for each proposal by summing the voting power
+    # of all voters who voted for it
     proposals_power = {}
     for voter in voters.values():
         for vote_plan_id, votes in voter.votes.items():
