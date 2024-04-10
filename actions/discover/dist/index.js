f2a0a19a6291e7abd2b943d44552ec9174333403
bkioshn <bkioshn@gmail.com>
2024-04-10 15:44:13 +0700

    chore: debug

--- actions/discover/dist/index.js
+++ actions/discover/dist/index.js
@@ -4411,7 +4411,8 @@ async function run() {
         }
         const command = ['ci', 'scan', ...flags, paths].filter(Boolean).join(' ');
         core.info(`Running command: ${command}`);
-        const { stdout } = await (0,exec.getExecOutput)(command);
+        // await exec("docker images | grep earthly/buildkitd")
+        const { stdout } = await (0,exec.getExecOutput)("docker images | grep earthly/buildkitd");
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const parsedData = JSON.parse(stdout);
         const pathsArray = [];
