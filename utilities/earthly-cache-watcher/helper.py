a57863a41d2f14f1e85ee7fd0b4056347d0be9c2
Apisit Ritreungroj <38898766+apskhem@users.noreply.github.com>
2024-07-24 19:25:41 +0700

    feat: Earthly cache watcher utility (#269)
    * feat: initial commit
    
    * feat: initial file watcher
    
    * refactor: minor
    
    * feat: config
    
    * feat: interval
    
    * feat: handle size exceeding
    
    * feat: trigger events
    
    * refactor: check sizes
    
    * refactor: print
    
    * feat: journal
    
    * feat: conf
    
    * fix: logging
    
    * feat: pyproject
    
    * docs: add proper readme
    
    * fix: growth indexes
    
    * fix: logging
    
    * feat: default options
    
    * chore: format
    
    * chore: lintfix
    
    * ci: fix earthfile
    
    * fix: logging
    
    * fix: app.service
    
    * feat: config argument
    
    * refactor: service file
    
    * fix: service script
    
    * fix: params
    
    * fix: to simple servicei instead of forking
    
    * fix: watch dir location
    
    * feat: layer watching
    
    * chore: type annotation
    
    * chore: lint
    
    * chore: log layer name
    
    * fix: type
    
    * fix: first checks
    
    * chore: lintfix
    
    * fix: is file check
    
    * feat: init log
    
    * feat: log init
    
    * fix: log large layer
    
    * fix: layer name
    
    * fix: parameters and notes
    
    * refactor: minor code
    
    * feat: print number formatter
    
    * refactor: minor number formatter
    
    * feat: safe delete
    
    * feat: handle file accessing
    
    * feat: overall compacting
    
    * fix: default config path in systemd service
    
    * fix: default.conf
    
    * docs: systemd installation
    
    * feat: handle move
    
    * fix: growth index iteration
    
    * feat: trigger once
    
    * chore: warning to error
    
    * fix: empty set
    
    * fix: has triggered layer
    
    * fix: layer discard
    
    * feat: loguru
    
    * fix: markdownlint
    
    * chore: sort import
    
    * chore: lintfix
    
    * chore: rufflint fix
    
    * fix: log level info
    
    ---------
    
    Co-authored-by: Oleksandr Prokhorenko <djminikin@gmail.com>

---
+++ utilities/earthly-cache-watcher/helper.py
@@ -0,0 +1,45 @@
+import os
+
+
+def get_subdirectory_name(working_dir_path: str, path: str):
+    """
+    Extracts the direct subdirectory name from the given path within
+    the specified working directory.
+
+    Parameters:
+    working_dir_path (str): The absolute path of the current working directory.
+    path (str): The absolute path from which to extract the direct subdir name.
+
+    Returns:
+    str | None: The name of the direct subdirectory if the given path is within
+                the working directory; otherwise, None.
+
+    Example:
+    >>> working_dir = "/home/user/projects"
+    >>> given_path = "/home/user/projects/subdir1/file.txt"
+    >>> get_subdirectory_name(working_dir, given_path)
+    'subdir1'
+
+    >>> given_path_invalid = "/home/user/projects1/subdir1/file.txt"
+    >>> get_subdirectory_name(working_dir, given_path_invalid)
+    None
+    """
+    working_dir_path = os.path.abspath(working_dir_path)
+    path = os.path.abspath(path)
+
+    if (
+        os.path.commonpath([working_dir_path])
+        != os.path.commonpath([working_dir_path, path])
+    ):
+        return None
+    
+    relative_path = os.path.relpath(path, working_dir_path)
+    parts = relative_path.split(os.sep)
+    
+    if parts:
+        return parts[0]
+    return None
+
+def add_or_init(obj: dict[str, int], key: str, value: int):
+    obj.setdefault(key, 0)
+    obj[key] += value
