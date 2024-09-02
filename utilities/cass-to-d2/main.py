4cbda4d6f73e17349ccd998579081d80176cb988
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-09-02 21:42:19 +0700

    feat: file management

--- utilities/cass-to-d2/main.py
+++ utilities/cass-to-d2/main.py
@@ -3,7 +3,8 @@ import os
 import re
 
 class Table:
-    def __init__(self) -> None:
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
         self.name = ""
         self.desc = ""
         self.fields: list[Field] = []
@@ -13,7 +14,8 @@ class Table:
         # format tooltip
         f_tooltip_lines: list[str] = []
         if self.desc:
-            f_tooltip_lines.append(self.desc)
+            f_tooltip_lines.append(f"\t\t-- {self.desc}")
+            f_tooltip_lines.append("\n")
 
         # format fields
         f_field_lines: list[str] = []
@@ -25,17 +27,17 @@ class Table:
           constraint_keys: list[str] = []
 
           if field.name in self.pk:
-              constraint_keys.append("P")
+              constraint_keys.append("PK")
 
           f_field_lines.append(field.to_d2_format(constraint_keys))
           f_tooltip_lines.append(f"{field.name} -- {field.comment}")
 
         return "\n".join([
             self.name + ": {",
-            "\t\tshape: sql_table",
-            "\t\ttooltip: |md",
-            "\n\t\t\t\t".join(f_tooltip_lines),
-            "\t\t|",
+            "\tshape: sql_table",
+            "\ttooltip: |md",
+            "\n\t\t".join(f_tooltip_lines),
+            "\t|",
             "",
             "\n".join(f_field_lines),
             "}"
@@ -51,9 +53,9 @@ class Field:
         return self.name == "" or self.type == ""
     
     def to_d2_format(self, constraint_keys: str) -> str:
-        f_constraints = " {constraint: " + ", ".join(constraint_keys) + "}" if len(constraint_keys) else ""
+        f_constraints = " {constraint: [" + "; ".join(constraint_keys) + "]}" if len(constraint_keys) else ""
 
-        return "\t\t" + self.name + f": ({self.type})" + f_constraints
+        return "\t" + self.name + f": {self.type}" + f_constraints
 
 def extract_src(src_dir: str) -> list[Table]:
     if not os.path.isdir(src_dir):
@@ -68,7 +70,7 @@ def extract_file(file_path: str) -> Table:
     f = open(file_path, "r")
     lines = f.readlines()
 
-    table = Table()
+    table = Table(extract_filename_without_ext(file_path))
 
     for line in lines:
         if line.strip() == "":
@@ -119,11 +121,22 @@ def extract_file(file_path: str) -> Table:
   
     return table
 
+def extract_filename_without_ext(path: str) -> str:
+    base_name = os.path.basename(path)
+    file_name, _ = os.path.splitext(base_name)
+    return file_name
+
+def write_to_file(dir_path: str, file_name: str, content: str):
+    os.makedirs(os.path.dirname(dir_path), exist_ok=True)
+    
+    with open(f"{dir_path}/{file_name}.txt", "w") as file:
+        file.write(content)
+
 def main():
     if len(sys.argv) != 3:
         raise Exception("The command requires two arguments to execute <src-dir> and <out-dir>.")
 
-    [ prog, src_dir, out_dir ] = sys.argv
+    [ _, src_dir, out_dir ] = sys.argv
 
     abs_src_dir = os.path.abspath(src_dir)
     abs_out_dir = os.path.abspath(out_dir)
@@ -131,7 +144,7 @@ def main():
     tables = extract_src(abs_src_dir)
 
     for table in tables:
-        print(table.to_d2_format())
+        write_to_file(abs_out_dir, table.file_name, table.to_d2_format())
 
 if __name__ == "__main__":
     main()
